877589c22bcb2ccabf6d9de4a5dd20b56f14fd40
Ilya Zatcepilov <deadburn1395@gmail.com>
2025-09-01 14:50:25 +0500

    refactor

---
+++ src/widgets/WindDataPanel/model/windDataPanelModel.ts
@@ -0,0 +1,13 @@
+import { useWindStore } from '@entities/WindData';
+import { useMemo } from 'react';
+import { computeWindMetrics, type WindMetrics } from '../lib/metrics';
+
+export function useWindDataPanelMetrics(): WindMetrics {
+  const framesByHeight = useWindStore((s) => s.framesByHeight);
+  const heights = useWindStore((s) => s.heightOrder);
+  const frameIndex = useWindStore((s) => s.frameIndex);
+
+  return useMemo(() => computeWindMetrics(framesByHeight, heights, frameIndex), [framesByHeight, heights, frameIndex]);
+}
+
+
